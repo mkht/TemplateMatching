cc564558c05608cce8ac4d07e01add978fa118d9
mkht <mkht@users.noreply.github.com>
2018-01-28 01:25:36 +0900

    Add sample

---
+++ Sample/sample.ps1
@@ -0,0 +1,12 @@
+﻿$Module = Join-Path (Split-Path $PSScriptRoot -Parent) '\TemplateMatching\PSModule\TemplateMatching\TemplateMatching.psd1'
+$TargetImage = Join-Path $PSScriptRoot '\target.png'
+$TemplateImage = Join-Path $PSScriptRoot '\template.png'
+
+if(-Not (Test-Path $Module)){
+    Write-Warning 'Module not found.'
+    return
+}
+
+Import-Module $Module -Force -Verbose
+
+Invoke-TemplateMatching -Target $TargetImage -Template $TemplateImage
