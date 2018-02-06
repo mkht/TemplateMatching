40d12b620f88a1701f54a80dabb4da8efae6da61
mkht <mkht@users.noreply.github.com>
2018-02-06 23:28:52 +0900

    Add publish script (PowerShell Gallery)

---
+++ publish.ps1
@@ -0,0 +1,28 @@
+﻿Param(
+    [string] $NuGetApiKey
+)
+
+$ErrorActionPreference = 'Stop'
+
+$ModuleRoot = Join-Path $PSScriptRoot '\TemplateMatching\PSModule\TemplateMatching'
+$ModuleManifest = Join-Path $ModuleRoot '\TemplateMatching.psd1'
+
+if (-Not (Test-Path $ModuleManifest)) {
+    Write-Error "Can not find module manifest file in $ModuleRoot"
+    return
+}
+else {
+    Test-ModuleManifest $ModuleManifest >$null
+}
+
+if (-Not $NuGetApiKey) {
+    $NuGetApiKey = Read-Host 'Enter NuGet Api Key'
+}
+
+$Paramter = @{
+    Path        = $ModuleRoot
+    NuGetApiKey = $NuGetApiKey
+}
+
+#Publish
+Publish-Module @Paramter -Verbose
